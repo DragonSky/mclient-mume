c28c6c98eb05fae78f0e2b3db580990049d6ced0
kwilson@barney.physics.sc.edu <kwilson@barney.physics.sc.edu@962f5b74-7ec1-11dd-90a9-6306d3ffb32f>
2008-10-16 20:42:08 +0000

    Need boost::python to compile this now, unless you comment out the include. I'll add a define for that. Also, added simple proxy support to the socket.

--- trunk/simple_client/src/ClientSocket.cpp
+++ trunk/simple_client/src/ClientSocket.cpp
@@ -1,7 +1,17 @@
 #include "ClientSocket.h"
 
+#include <QNetworkProxy>
+
 
 ClientSocket::ClientSocket(QObject* parent) : QTcpSocket(parent) {
+    _proxy.setType(QNetworkProxy::NoProxy);
+    //_proxy.setType(QNetworkProxy::Socks5Proxy);
+    //_proxy.setHostName("proxy.example.com");
+    //_proxy.setPort(1080);
+    //_proxy.setUser("username");
+    //_proxy.setPassword("password");
+    //QNetworkProxy::setApplicationProxy(proxy)
+    setProxy(_proxy);
 }
 
 
