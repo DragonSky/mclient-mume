af2681486be9e9f1cd3c548f8202404db236c63d
kwilson@barney.physics.sc.edu <kwilson@barney.physics.sc.edu@962f5b74-7ec1-11dd-90a9-6306d3ffb32f>
2008-10-16 08:39:51 +0000

    Commented a little

--- trunk/simple_client/src/main.cpp
+++ trunk/simple_client/src/main.cpp
@@ -11,12 +11,20 @@ int main(int argc, char** argv) {
     QApplication app(argc, argv);
     QMainWindow mw;
 
+    // Create a few cool things: powwow, the client, (the mapper), ... 
+    PowwowWrapper* pw = PowwowWrapper::Instance();
     ClientWidget* cw = new ClientWidget();
-    cw->connectToHost("mume.org", 4242);
+    
+    // Add dockwidgets to the mainwindow here, after everything's ready
     mw.setCentralWidget(cw);
     mw.show();
+    
+    // Time for some magic
+    pw->connectToHost("mume.org", 4242);
 
     int retval = app.exec();
+    
+    // Cleanup
     PowwowWrapper::Instance()->Delete();
     return retval;
 }
