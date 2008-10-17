a9a6717263250240227bd64c1223dfc9e7b22b02
nschimme <nschimme@962f5b74-7ec1-11dd-90a9-6306d3ffb32f>
2008-10-17 00:33:01 +0000

    fixing repo problem, adding client and mapper directories which should have been in the last commit

---
+++ trunk/src/client/wrapper_cmd.cpp
@@ -0,0 +1,31 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Nils Schimmelmann   *
+ *   nschimme@gmail.com   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "wrapper_cmd.h"
+
+#include "PowwowWrapper.h"
+
+
+void suspend_powwow() {
+
+}
+
+
+int wrapper_get_keybind(char *seq) {
+    return PowwowWrapper::self()->getKeyBind(seq); 
+}
+
+
