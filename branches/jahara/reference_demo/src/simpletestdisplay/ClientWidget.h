845ec7e70e4a455ee4092621e76c21cfa9a7257d
nschimme <nschimme@962f5b74-7ec1-11dd-90a9-6306d3ffb32f>
2009-06-12 05:39:48 +0000

    Proof of concept of passing in references with threads on windows

---
+++ branches/jahara/reference_demo/src/simpletestdisplay/ClientWidget.h
@@ -0,0 +1,42 @@
+#ifndef CLIENTWIDGET_H
+#define CLIENTWIDGET_H
+
+#include <QWidget>
+
+class ClientLineEdit;
+class ClientTextEdit;
+class SimpleTestDisplay;
+
+class QTcpSocket;
+class QVBoxLayout;
+
+class ClientWidget : public QWidget {
+    Q_OBJECT
+    
+    public:
+        ClientWidget(QString s, SimpleTestDisplay* st, QWidget* parent=0);
+        ~ClientWidget();
+
+    public slots:
+        void sendUserInput();
+
+
+    protected:
+        ClientTextEdit* _textEdit;
+        ClientLineEdit* _lineEdit;
+        QTcpSocket* _socket;
+        QVBoxLayout* _layout;
+        SimpleTestDisplay* _st;
+
+    private:
+        QString _session;
+
+
+    signals:
+        void receivedInput();
+	void sendToSocketData(const QByteArray&);
+	void connectToHost();
+};
+
+
+#endif /* CLIENTWIDGET_H */
